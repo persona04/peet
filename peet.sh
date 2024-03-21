948df6166c56db184739a6b9448b12ac0db60880
Özer Öztürk <ozer.ozturk52@hotmail.com>
2024-03-21 10:56:41 -0400

    feat: Add user info enumeration function

--- peet.sh
+++ peet.sh
@@ -1,2 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 
+function get_user_info(){
+
+echo -e "USER INFO"
+
+echo -e "whoami  : $(whoami)"
+echo -e "id      : $(id)"
+
+groups=("docker" "lxd" "sudo" "admin" "wheel" "shadow" "root" "adm" "auth" "video" "disk" "staff")
+my_id=$(id)
+
+declare -a danger
+declare -i flag
+flag=0
+
+# check for dangerous  groups.
+for group in ${groups[@]}; do
+ if [[ -n "$(grep -iE  ${group} <<< ${my_id} )" ]]; then
+  danger+=("${group}")
+  flag=1
+  fi
+done
+
+if [[ $flag -eq 1 ]]; then
+ echo -e "Check this groups: ${danger[@]}"
+fi
+}
+
+
+get_user_info
